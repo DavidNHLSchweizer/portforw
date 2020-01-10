cbdf027f5c575f878664fe68c7fe864ed650e2e0
David Schweizer <David.Schweizer@nhlstenden.com>
2020-01-10 11:27:44 +0100

    consolidation

--- PythonApplication1/portForwarder.py
+++ PythonApplication1/portForwarder.py
@@ -4,8 +4,12 @@ import requests
 import argparse
 import socket
 
-FAVICONREQUEST = "/favicon.ico"
-PORT = 65432
+def isFAVICONRequest(requestPath):
+    FAVICONREQUEST = "/favicon.ico"
+    return requestPath==FAVICONREQUEST
+
+HTTPNOTFOUND = 404
+DEFAULTPORT = 65432
 def parseArguments(defaultPort):
     parser = argparse.ArgumentParser(description='simple port forwardert')
     parser.add_argument('--p',default=defaultPort,type=int,help='the port to use [{}]'.format(defaultPort))
@@ -19,26 +23,29 @@ class clientServerInfo(object):
 
 class ForwardingHandler(http.server.SimpleHTTPRequestHandler):
     knownClientServers = []
-    CurrentClientIndex = 0
-    def selectClientServer(self):
-        index = ForwardingHandler.CurrentClientIndex
-        ForwardingHandler.CurrentClientIndex = (ForwardingHandler.CurrentClientIndex + 1) % len(ForwardingHandler.knownClientServers)        
+    NextClientIndex = 0
+
+    def getNextClientServerInfo(self):
+        index = ForwardingHandler.NextClientIndex
+        ForwardingHandler.NextClientIndex = (ForwardingHandler.NextClientIndex + 1) % len(ForwardingHandler.knownClientServers)
         return ForwardingHandler.knownClientServers[index]
-    def getResponseFromServer(self):
-        client = self.selectClientServer()
+
+    def getResponseFromClientServer(self):
+        client = self.getNextClientServerInfo()
         return requests.get('http://{}:{}'.format(client.host, client.port))
+
     def sendResponse(self, response):
         self.send_response(response.status_code)
         self.send_header("Content-type", "text/html")
         self.send_header("Content-length", len(response.text))
         self.end_headers()
         self.wfile.write(str.encode(response.text))     
+
     def do_GET(self):     
-        if self.path==FAVICONREQUEST:
-            self.send_response(404)
+        if isFAVICONRequest(self.path)
+            self.send_response(HTTPNOTFOUND)
             return
-        ForwardingHandler.recursive = True
-        response = self.getResponseFromServer()
+        response = self.getResponseFromClientServer()
         if response:
             self.sendResponse(response)
 
@@ -46,15 +53,17 @@ class PortForwarder(object):
     def __init__(self, host, port):
         self.server = socketserver.TCPServer((host, port), ForwardingHandler)
         print("serving at port", port)
+
     def addClientServer(self, host, port):
         ForwardingHandler.knownClientServers.append(clientServerInfo(host,port))
+        
     def run(self):
         for i in range(len(ForwardingHandler.knownClientServers)):
             client = ForwardingHandler.knownClientServers[i]
             print('server index: {} port: {}.'.format(i, client.port))
         self.server.serve_forever()
 
-port = parseArguments(PORT)
+port = parseArguments(DEFAULTPORT)
 PF = PortForwarder('127.0.0.1', port)
 PF.addClientServer('127.0.0.1', 8000)
 PF.addClientServer('127.0.0.1', 8001)
